88840f6c88b3822fd936c8cf96a93b007d7cf76f
DForDespair <amoghaddam.business@gmail.com>
2022-03-21 14:14:05 -0400

    added config.py, fixed LatestPrice issue

---
+++ config.py
@@ -0,0 +1,12 @@
+config = {
+    'sandboxKey':
+    r'Tsk_0c13a792b3984153a0aa281086d284f9',
+    'apiKey':
+    r'<INSERT API KEY HERE>',
+    'tickers': [
+        'aapl', 'intc', 'meta', 'chpt', 'curo', 'para', "kplt", "tsla",
+        "googl", "twtr"
+    ],
+    'imageDir':
+    r'<INSERT DIRECTORY HERE>'
+}
